55c200233ec30e40a6317c14813eb603e6b78a17
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-01-06 22:36:57 +0330

    update File(`source.dart`): add name to source.

--- implementations/Dart/lexer/source.dart
+++ implementations/Dart/lexer/source.dart
@@ -2,14 +2,19 @@ import 'dart:io';
 
 class Source {
   final String src;
-  const Source(this.src);
+  final String? name;
+  const Source(this.src, [this.name]);
 
   static Future<Source?> from(String path) async {
     final file = File(path);
     if (! await file.exists())
       return null;
 
-    return Source(await file.readAsString());
+    return Source(await file.readAsString(), _getFileName(path));
+  }
+
+  static String _getFileName(String path) {
+    return path.substring((path.lastIndexOf('/') + 1).clamp(0, path.length - 1), path.length);
   }
 
   String chunk(int start, [int? end]) {
