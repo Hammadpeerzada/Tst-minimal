55ef4e24187d5f3a6416adcb5eca467643ae9a30
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2026-01-06 23:02:01 +0330

    new File(`const-errors.dart`): new errors constant file.
    including ansi colors for colord errors

---
+++ implementations/Dart/constants/const-errors.dart
@@ -0,0 +1,18 @@
+class E {
+  // Bright colors for high visibility
+  static const
+    errorType    = "\x1b[91m",  // Bright Red for error type
+    message      = "\x1b[37m",  // Bright Gray for message
+    name         = "\x1b[39m",  // White for name
+    context      = "\x1b[90m",  // Bright Black/Gray for context
+    location     = "\x1b[39m",  // Bright Blue for location
+    caret        = "\x1b[32m",  // Bright Green for caret
+    punctuation  = "\x1b[93m",  // Yellow for punctuation
+    symbols      = "\x1b[94m",  // Bright Cyan for symbols
+    highlight    = "\x1b[92m",  // Bright Green for highlighted text
+    bold         = "\x1b[1m",
+    ubold        = "\x1b[21m",
+    underline    = "\x1b[4m",
+    uunderline   = "\x1b[24m",
+    reset        = "\x1b[0m";
+}
